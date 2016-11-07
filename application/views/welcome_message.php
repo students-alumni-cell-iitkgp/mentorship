98797bb4b4ab70327c451c2e2292d6997bc032b2
Harshit Mishra <harshitmishra1331@gmail.com>
2016-11-07 21:03:19 +0530

    Corrected Redirect URl  (#10)
    * Updated changes
    
    * session for login (#3)
    
    * Update welcome_message.php (#4)
    
    * Updating page after Error and Login Page
    
    * Updating Page after Login error
    
    * Correcting redirect address
    
    * correcting redirecton url
    
    * correcting redirection url
    
    * correcting redirectional url

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<link rel="icon" href="../../img/p.png">
+	<link rel="icon" href="img/p.png">
 	<title>Welcome to Mentorship-Portal</title>
-	<link rel='stylesheet' href='../../css/social_icon.css'>
+	<link rel='stylesheet' href='css/social_icon.css'>
 
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<!-- Generic page styles -->
@@ -155,22 +155,22 @@ input[type=number]::-webkit-outer-spin-button {
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox" >
     <div class="item active">
-      <img src="../../img/mentorslider.jpg" style="width: 100vw; height: 450px;">
+      <img src="img/mentorslider.jpg" style="width: 100vw; height: 450px;">
      
     </div>
 
     <div class="item">
-      <img src="../../img/mentorslidertwo.jpg" style="width: 100vw; height: 450px;">
+      <img src="img/mentorslidertwo.jpg" style="width: 100vw; height: 450px;">
       
     </div>
 
     <div class="item">
-      <img src="../../img/mentorshipsliderthree.png" style="width: 100vw; height: 450px;">
+      <img src="img/mentorshipsliderthree.png" style="width: 100vw; height: 450px;">
       
     </div>
 
     <div class="item">
-      <img src="../../img/mentorshipsliderfour.jpg"  style="width: 100vw; height: 450px;">
+      <img src="img/mentorshipsliderfour.jpg"  style="width: 100vw; height: 450px;">
       
     </div>
   </div>
@@ -298,7 +298,7 @@ input[type=number]::-webkit-outer-spin-button {
 			<br>
 			<div class="tab-content" style="width:100%">
 				<div id="home" class="tab-pane fade in active">
-					<img src="../../img/p.jpg" style="position:relative;" width="100%" height="100" class="img img-rounded" />
+					<img src="img/p.jpg" style="position:relative;" width="100%" height="100" class="img img-rounded" />
 					<br/><br/>
                  
 					
@@ -332,45 +332,45 @@ input[type=number]::-webkit-outer-spin-button {
 					<div class ="al">
 						<div class="row" >
 							<div class="col-md-8" style="background-color:;">
-								<img src="../../img/a.png" alt="" id="quotes" />
+								<img src="img/a.png" alt="" id="quotes" />
 
 								<p>    Mentorship is crucial for any individual whether a student or professional, and its importance is essential especially during the formative years in college. I signed up for this program with the objective to gain perspective and seek help from our experienced and extremely friendly alumni. His guidance helped me think and understand about the outside world, gain industry knowledge and valuable fundas which helped me perform well in college and outside. They know what is valued more when it comes to the professional world, hence I always prioritized my mentors advice - specifically maintaining a good CGPA. Interacting and keeping in touch with my mentor also helped improving my networking skills and connecting to more people.   
 									<br>
-									<img src="../../img/b.png" alt="" id="quotes" class="pull-right" />
+									<img src="img/b.png" alt="" id="quotes" class="pull-right" />
 
 									
 								</p>
 							</div>
 							
-							<div class="col-md-4" >	<br><br><img src="../../img/t2.jpg" class="img img-circle" width="200px" height="200px">
+							<div class="col-md-4" >	<br><br><img src="img/t2.jpg" class="img img-circle" width="200px" height="200px">
 								<br><br>	<b style="position:relative;left:60px;">Ankit Gupta</b>
 							</div>
 						</div><hr>
 						<div class="row">
 							<div class="col-md-8">
-								<img src="../../img/a.png" alt="" id="quotes" />
+								<img src="img/a.png" alt="" id="quotes" />
 								<p>      The Alumni Student mentorship program is a nice initiative by the Students’ Alumni Cell. In today’s time a direction and support from an esteemed mentor can go a long way in helping someone. It was a great experience for me as I got to learn a lot from my mentor who provided me with insight into the world after college as well as their experience as a student and in the professional sphere. Getting mentorship from an Alumni is a great thing as they can provide better help as they have been a part of the same institution as you."
 									<br>
-									<img src="../../img/b.png" alt="" id="quotes" class="pull-right" />
+									<img src="img/b.png" alt="" id="quotes" class="pull-right" />
 
 									
 								</p></div>
 								
-								<div class="col-md-4"><br><img src="../../img/t3.jpg" class="img img-circle"  width="200px" height="200px"><br><br><b style="position:relative;left:60px;">Pranshu Agrawal</b></div>
+								<div class="col-md-4"><br><img src="img/t3.jpg" class="img img-circle"  width="200px" height="200px"><br><br><b style="position:relative;left:60px;">Pranshu Agrawal</b></div>
 							</div><hr>
 							<div class="row">
 								<div class="col-md-8">
-									<img src="../../img/a.png" alt="" id="quotes" />
+									<img src="img/a.png" alt="" id="quotes" />
 									<p>The Student Alumni Mentorship Programme is probably one of the best
 										initiatives taken by our institution in recent years. Through my
 										association with it, I can only say that although the path was clear for
 										me, it was dark. My mentor came in the form a torchlight.
 										<br>
-										<img src="../../img/b.png" alt="" id="quotes" class="pull-right" />	
+										<img src="img/b.png" alt="" id="quotes" class="pull-right" />	
 									</p>
 								</div>
 								
-								<div class="col-md-4"><img src="../../img/t1.jpg" class="img img-circle" width="200px" height="200px"><br><br><b style="position:relative;left:60px;">Gobind Singh</b></div>
+								<div class="col-md-4"><img src="img/t1.jpg" class="img img-circle" width="200px" height="200px"><br><br><b style="position:relative;left:60px;">Gobind Singh</b></div>
 							</p>
 						</div>
 					</div>
