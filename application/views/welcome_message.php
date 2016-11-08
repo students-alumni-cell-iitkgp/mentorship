094316371e0cdc647d9984b5289e2ed0de84725c
harshit2575 <harshitmishra1331@gmail.com>
2016-11-08 14:53:46 +0530

    Corrected Prezi and Logout view for session signedin user

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -306,12 +306,13 @@ input[type=number]::-webkit-outer-spin-button {
 						The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell as an initiative to create a platform for the exchange of ideas and experience between the alumni and the current students of IIT Kharagpur.<br><br>
 						With an ocean of possibilities available, students often find it difficult to decide their way out into the future. There are lots of instances where students need  a mentor who can counsel and guide them through. The alumni can improve students' insight into various aspects of life.  Their journey and personal experiences can help the students and motivate them through this pivotal phase of life. Under this programme, we invite our alumni from across the world and from different fields to interact with the students and help them build their network and gain new perspective on the possibilities open to them after graduation.
 					</div>
-				</div>
+				
                                <br>
 
 					  <iframe id="iframe_container" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""
 					   src="https://prezi.com/embed/4zvbjzz_cb-w/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=1&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE1vbGxLeUtRdXkycVhRdXRFSWpEMFgxaDFqbVU3R256Q051MTVVNkxmOD0&amp;landing_sign=10ozq-9rCOuctWoHR42bTHCfMKCM7iXkyQfYmXAcPPs" 
 					   width="100%" height="400" frameborder="0"></iframe>
+					   </div>
 				<br><br>
 				<!-- Add tabs here-->
 				<div id="menu1" class="tab-pane fade">
