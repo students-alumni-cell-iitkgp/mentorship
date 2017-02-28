61b2fcd74bfa9c0d2c5c978a59865d10cd7b270f
harshit2575 <harshitmishra1331@gmail.com>
2017-02-28 21:58:55 +0530

    Adding Carousel

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -235,7 +235,7 @@
    <!-- Indicators -->
    <ol class="carousel-indicators">
      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-     <!-- <li data-target="#myCarousel" data-slide-to="1"></li> -->
+     <li data-target="#myCarousel" data-slide-to="1"></li>
      <!-- <li data-target="#myCarousel" data-slide-to="2"></li> -->
      <!-- <li data-target="#myCarousel" data-slide-to="3"></li> -->
    </ol>
@@ -243,14 +243,12 @@
    <!-- Wrapper for slides -->
    <div class="carousel-inner" role="listbox" >
      <div class="item active">
-       <img src="img/slider_img.png" style="width: 100vw;">
-      
-     </div>
+       <img src="img/slider1.jpg" style="width: 100vw;">
+       </div>
  
-     <!-- <div class="item"> -->
-       <!-- <img src="img/mentorslidertwo.jpg" style="width: 100vw; height: 450px;"> -->
-       
-     <!-- </div> -->
+     <div class="item">
+       <img src="img/slider2.jpg" style="width: 100vw;">
+       </div>
  
      <!-- <div class="item"> -->
        <!-- <img src="img/mentorshipsliderthree.png" style="width: 100vw; height: 450px;"> -->
